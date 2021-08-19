985046bc7c0b2b60f6f22f01905e1b2c436df740
Michel John <michel.john@hotmail.com>
2021-08-19 20:09:29 -0300

    Add files via upload

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+// config-overrides.js
+module.exports = {
+  webpack: function (config, env) {
+    return config;
+  },
+  jest: function (config) {
+    return config;
+  },
+  devServer: function (configFunction) {
+    return function (proxy, allowedHost) {
+      return config;
+    };
+  },
+  paths: function (paths, env) {
+    return paths;
+  },
+}
